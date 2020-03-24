7294ae4d7b6f4aa6afb4fd839e5431274cab6786
xthirtysix <ggalliosha@gmail.com>
2020-03-24 10:09:55 +0300

    Оживление слайдеров

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,39 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+(function () {
+  window.swiper('.trainers__container', {
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 32,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 30
+      },
+      1200: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        spaceBetween: 40
+      }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next--trainers',
+      prevEl: '.swiper-button-prev--trainers',
+    }
+  });
 
-pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+  window.swiper('.feedback__container', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 55,
+    navigation: {
+      nextEl: '.swiper-button-next--feedback',
+      prevEl: '.swiper-button-prev--feedback',
+    }
+  });
+})();
